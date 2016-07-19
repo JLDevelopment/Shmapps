be9220278c61ac1128d014fe75d31b01aaca41e0
John Landa <Landa048@umn.edu>
2016-07-19 16:57:43 -0500

    Initial Commit

---
+++ Shmapps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shmapps
+//
+//  Created by John Landa on 7/19/16.
+//  Copyright © 2016 John Landa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
